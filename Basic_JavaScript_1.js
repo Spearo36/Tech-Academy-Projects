f85eabfe16d4ee24e5cab9847f68c1d1a6971af6
Eric <40644232+Spearo36@users.noreply.github.com>
2019-12-10 13:39:07 -0800

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,44 @@
+window.alert("Hello - Pop Up");
+document.write("Hello, External File!");
+
+// Javascript Variable to string
+
+var A = "This is my first string"
+document.write(A);
+
+
+// Assignment window.alert method for var to string value
+
+    var B = "First Var String Alert";
+    window.alert(B);
+
+// Escaping
+document.write("I asked the instructor, \"How do I write line breaks in my code\" the Instructor\'s responded with")
+
+
+// Concatenate
+document.write("\"I never saw a wild thing sorry for itself." + "  A small bird will drop frozen dead from a bough\" -D.H. Lawrence")
+
+
+// Concatenate String Value 
+var C = "\"Live for something" + " rather then die for nothing\" -George S. Patton Jr" + "A famous quote I enjoy"
+document.write(C);
+
+
+// Multiple Variables 
+var Manufacturer = "Chevy", Truck = "Silverado", Engine = "Duramax", SUV = "Tahoe", Car = "Corvette";
+var Manufactuer = Manufacturer.fontcolor("blue");
+var Truck = Truck.fontcolor("Grey");
+var Engine = Engine.fontcolor("Yellow");
+var SUV = SUV.fontcolor("Green");
+var Car = Car.fontcolor("Red");
+document.write(Car);
+
+
+//Expressions
+document.write(198 + 198);
+
+
+
+
+
